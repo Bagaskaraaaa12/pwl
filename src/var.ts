9578a36ac715878c3b53fc9414f3a3f5787c808e
bgas_k <h1101241023@student.untan.ac.id>
2026-02-25 02:13:11 +0000

    update

---
+++ src/var.ts
@@ -0,0 +1,11 @@
+let nama: string = "Virsya";
+let umur: number = 1945;
+let aktif: boolean = true;
+let semester: number = 2;
+
+console.log(`Nama   : ${nama}`); 
+console.log(`Umur   : ${umur}`); 
+console.log(`Aktif  : ${aktif}`);
+console.log(`Semester  : ${semester}`);
+
+console.log(`Nama : ${nama}, Umur : ${umur}, Semester : ${semester}. Apakah aktif? ${true}`);
